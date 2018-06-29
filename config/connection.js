7614fc180d88f18a5423ef17008927cdb7497762
Yirklum <yirklum@aol.com>
2018-06-28 23:17:45 -0400

    deploying to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Require mysql
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = sql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
-  password: "",
+  password: "hacktheplanet",
   database: "burgers_db"
-});
+  });
+};
 
 // Set up connection
 connection.connect(function(err) {
