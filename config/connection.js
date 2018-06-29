349aaf6eff96f6420b297a6dbaafe08ec4654ae7
Yirklum <yirklum@aol.com>
2018-06-29 00:09:06 -0400

    fixing heroku connection

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = sql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
   host: "localhost",
